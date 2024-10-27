507fdff0aa3ea1472e4a59365fe3de3633879be9
Andrei Lavrenov <andrei@lavrenov.io>
2024-10-27 22:52:06 +0100

    more configuration

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -288,7 +288,7 @@ $settings['config_sync_directory'] = '../config/sync';
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = '';
+$settings['hash_salt'] = file_get_contents('../config/salt.txt'); # generated on 2024-10-27 with Bitwarden's Password Generator
 
 /**
  * Deployment identifier.
@@ -764,7 +764,10 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
  *
  * @see https://www.drupal.org/docs/installing-drupal/trusted-host-settings
  */
-# $settings['trusted_host_patterns'] = [];
+$settings['trusted_host_patterns'] = [
+    '^.+\.ddev\.site$',  // Matches any subdomain of ddev.site
+    '^whitelabel\.lavrenov\.dev$',  // Matches exactly whitelabel.lavrenov.dev
+];
 
 /**
  * The default list of directories that will be ignored by Drupal's file API.
