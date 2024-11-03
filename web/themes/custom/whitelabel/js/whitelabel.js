a9c5095f86e51242a6ac11cce66e34bb7d8f65e6
Andrei Lavrenov <andrei@lavrenov.io>
2024-11-03 01:38:41 +0100

    whitelabel theme + config

---
+++ web/themes/custom/whitelabel/js/whitelabel.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * whitelabel behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.whitelabel = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
